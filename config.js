afb40d58a82c3dd57b4d0bc5232d8cad599e9953
caseyweb <caseywebsthedonn25@gmail.com>
2024-11-15 11:15:12 +0300

    CASEYRHODES💫

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "azQCTZya#Wwa9X4RDbyUlpyFA3N5foCpmgvh6hDx9FSneyslGNpc",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
